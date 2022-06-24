90f05899d64103c37bcb676ece854ea198e235a6
K3RN3L32 <alpege7@hotmail.com>
2022-06-24 21:46:18 +0300

    commit
    asd

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,7 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <link rel="icon" href="https://github.com/K3RN3L32/nextjs-blog-theme/blob/main/VestigeLogoVsmall.png?raw=true" sizes="128x128" type="image/png"/>
     </Head>
   );
 }
